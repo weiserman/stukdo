a946c4484c2dba6257569cd4efa8d5421e9496a1
weiserman <warreneiserman@gmail.com>
2016-09-18 18:19:11 +0200

    Added About Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'pages/home'
   root 'pages#home'
+
+  # routing for the about pages
+  get 'about' => 'pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
